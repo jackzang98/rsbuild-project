6916a7c76e667967e755c54b28148de57880479f
zangwenbiao <wenbiao.zang@innodealing.com>
2025-04-16 10:59:14 +0800

    feat: enhance project configuration and add contact route
    - Updated rsbuild.config.ts to include ForkTsCheckerWebpackPlugin for better TypeScript error handling in development.
    - Refactored Root component in src/routes/root/index.tsx for improved readability and structure.
    - Modified tsconfig.json to add baseUrl and paths for easier module resolution.
    - Added new contact route in src/routes/contact/index.tsx with functionality to display, edit, and delete contact details.

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,13 +1,37 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 export default defineConfig({
   plugins: [pluginReact()],
   output: {
     sourceMap: {
-      js: process.env.NODE_ENV === 'development' ? 'cheap-module-source-map' : 'source-map',
+      js:
+        process.env.NODE_ENV === 'development'
+          ? 'cheap-module-source-map'
+          : 'source-map',
       css: true,
     },
   },
+  tools: {
+    rspack(config) {
+      // 只在开发环境下使用
+      if (process.env.NODE_ENV === 'development') {
+        config.plugins?.push(
+          new ForkTsCheckerWebpackPlugin({
+            async: false, // 同步输出错误信息，终端立刻看到
+            typescript: {
+              diagnosticOptions: {
+                semantic: true,
+                syntactic: true,
+              },
+              mode: 'write-tsbuildinfo',
+              // 使用你的 tsconfig 文件
+              configFile: './tsconfig.json',
+            },
+          }),
+        );
+      }
+    },
+  },
 });
-  
